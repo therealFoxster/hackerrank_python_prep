e5c35aa2f9d744c10808733acd2ac461d8e14ec4
Foxster <huythefox@gmail.com>
2024-04-27 12:41:24 -0700

    Add set_mutations.py

---
+++ python_basic/easy/sets/set_mutations.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+input()
+A = set(map(int, input().split()))
+N = int(input())
+
+for i in range(N):
+    cmd, *arg = input().split()
+    B = set(map(int, input().split()))
+    if cmd == "intersection_update":
+        A.intersection_update(B)
+    elif cmd == "update":
+        A.update(B)
+    elif cmd == "symmetric_difference_update":
+        A.symmetric_difference_update(B)
+    elif cmd == "difference_update":
+        A.difference_update(B)
+
+print(sum(A))
